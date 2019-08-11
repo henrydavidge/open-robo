c4e8056c810583c2569ff2d9a5e81ec6bcf9d6e2
Henry D <henrydavidge@gmail.com>
2019-08-11 09:59:44 -0700

    display unrealized; keep trades open

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -4,6 +4,10 @@ import dt from 'datatables.net';
 import 'datatables.net-dt/css/jquery.dataTables.css';
 $.DataTable = dt;
 
+showTimestamps();
+maybeDisplayTrades();
+let unrealizedTable = null;
+
 browser.runtime.onMessage.addListener( (msg) => {
   console.log('Received message', msg);
   if (!msg || !msg.type) {
@@ -25,43 +29,79 @@ document.getElementById("refreshUnrealized").onclick = (event) => {
   });
 };
 
-document.getElementById("showTrades").onclick = (event) => {
+document.getElementById("invest").onclick = (event) => {
   browser.storage.local.get(['unrealizedCostBasis', 'portfolio'])
     .then( (obj) => {
       const df = new DataFrame(obj.unrealizedCostBasis.data);
       const cash = parseFloat(document.getElementById('cash').value);
-      const trades = getInvestments(df, cash, obj.portfolio);
+      const trades = getInvestments(df, cash, obj.portfolio)
+        .map(row => row.set('hide', 'Hide'));
+      saveTrades(trades.toArray());
       displayTrades(trades);
     });
 };
 
-document.getElementById('clearState').onclick = event => {
-  browser.storage.local.remove(['unrealizedCostBasis']);
+document.getElementById("clearTrades").onclick = event => {
+  clearTrades();
 };
 
-showTimestamps();
+document.getElementById("toggleUnrealized").onclick = event => {
+  if (unrealizedTable !== null) {
+    destroyDataTable("#unrealizedTable");
+    unrealizedTable = null;
+    document.getElementById("toggleUnrealized").textContent = "Show";
+  } else {
+    browser.storage.local.get(['unrealizedCostBasis'])
+      .then( obj => {
+        unrealizedTable = $('#unrealizedTable').DataTable( {
+          data: obj.unrealizedCostBasis.data,
+          columns: [
+            { title: "Ticker", data: "ticker" },
+            { title: "Value", data: "marketValue" },
+            { title: "Date", data: "date", render: d => new Date(d).toLocaleDateString("en-US") },
+          ],
+          searching: false,
+          paging: false,
+          order: [[0, 'asc']]
+        });
+        document.getElementById("toggleUnrealized").textContent = "Hide";
+      });
+  }
+}
+
+function saveTrades(data) {
+  browser.storage.local.set({ suggestedTrades: data });
+}
+
+function destroyDataTable(selector) {
+  if ($(selector).children().length === 0) {
+    return;
+  }
+
+  $(selector).DataTable().destroy();
+  $(selector).empty();
+}
 
 function showTimestamps() {
   browser.storage.local.get(['unrealizedCostBasis'])
     .then( obj => {
-      console.log(obj.unrealizedCostBasis.timestamp);
       const dateStr = obj.unrealizedCostBasis.timestamp;
-      console.log(dateStr);
       document.getElementById('unrealizedLastRefresh').textContent = dateStr;
     });
 }
 
 function getInvestments(df, cash, portfolio) {
+  const floorFn = n => cash > 0 ? Math.max(n, 0) : Math.min(n, 0);
   const withCat = df.map(row => row.set('category', getCategory(row.get('ticker'), portfolio)));
   const totalValue = df.stat.sum('marketValue');
   const deltaByCat = withCat.groupBy('category').aggregate(cat => cat.stat.sum('marketValue'), 'totalValue')
     .map(row => row.set('desiredValue', portfolio[row.get('category')].allocation * (totalValue + cash)))
-    .map(row => row.set('desiredDelta', Math.max(row.get('desiredValue') - row.get('totalValue'), 0)));
+    .map(row => row.set('desiredDelta', floorFn(row.get('desiredValue') - row.get('totalValue'))));
 
   const totalDelta = deltaByCat.stat.sum('desiredDelta');
   const categoriesToBuy = deltaByCat.map(row => row.set('delta', row.get('desiredDelta') * (cash / totalDelta)))
+    .filter(row => row.get('delta') != 0);
   return categoriesToBuy.map(row => row.set('ticker', chooseTicker(row.get('category'), portfolio)))
-    .filter(row => row.get('delta') > 0)
     .select('ticker', 'delta');
 }
 
@@ -79,10 +119,28 @@ function chooseTicker(category, portfolio) {
   return portfolio[category].tickers[0];
 }
 
+function maybeDisplayTrades() {
+  browser.storage.local.get(['suggestedTrades'])
+    .then( obj => {
+      if (obj.suggestedTrades) {
+        console.log(obj.suggestedTrades);
+        displayTrades(new DataFrame(obj.suggestedTrades));
+      } else {
+        clearTrades();
+      }
+    });
+}
+
 function displayTrades(tradesDf) {
+  console.log("Making trades table");
+  document.getElementById("investDiv").hidden = true;
+  document.getElementById("clearTradesDiv").hidden = false;
   $('#tradesTable').dataTable( {
-    data: tradesDf.map(row => row.set('hide', 'Hide')).toArray(),
-    columns: [ { title: 'Ticker' }, { title: 'Delta' }, { title: 'Action', className: 'hide' }],
+    data: tradesDf.toArray(),
+    columns: [ 
+      { title: 'Ticker' }, 
+      { title: 'Delta' }, 
+      { title: 'Action', className: 'hide' }],
     searching: false,
     paging: false,
     order: [[1, 'desc']]
@@ -91,10 +149,14 @@ function displayTrades(tradesDf) {
 
   $('.hide').click( (event) => {
     const el = $(event.target).parents('tr');
-    console.log(el);
-    console.log(table.row(el));
     table.rows(el).remove().draw();
+    saveTrades(table.rows().data().toArray());
   });
+}
 
-  $('#showTrades').click(event => table.destroy());
+function clearTrades() {
+  document.getElementById("investDiv").hidden = false;
+  document.getElementById("clearTradesDiv").hidden = true;
+  destroyDataTable("#tradesTable");
+  browser.storage.local.remove(['suggestedTrades']);
 }
