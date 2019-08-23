d8bf45f4c3e7d81ca76ae71234d20dde93d4dba6
Henry D <henrydavidge@gmail.com>
2019-08-22 20:50:36 -0700

    make realized responsive

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,76 +1,171 @@
 import jQuery from 'jquery';
 const $ = jQuery;
 
-let haveRefreshed = false;
+let haveRefreshedUnrealized = false;
+let haveRefreshedRealized = false;
 
 browser.runtime.onMessage.addListener( (msg) => {
   console.log('Received message', msg);
   if (!msg || !msg.type) {
     return;
   } else if (msg.type === 'expansion-complete') {
-    const unrealizedCostBasis = parseData();
+    const unrealizedCostBasis = parseUnrealizedCostBasis();
     browser.storage.local.set( { unrealizedCostBasis: unrealizedCostBasis });
   } else if (msg.type === 'refresh-unrealized') {
     try {
-      if (haveRefreshed) {
+      if (haveRefreshedUnrealized) {
         throw 'Can only refresh cost basis once between page refreshes';
       }
-      const tickers = parseTickers();
-      fetchTickerData().then(el => {
-        const unrealizedCostBasis = parseData(tickers, el);
-        console.log('New cost basis is', unrealizedCostBasis);
+      const tickers = parseTickers(true);
+      fetchUnrealizedCostBasis().then(el => {
+        const unrealizedCostBasis = parseUnrealizedCostBasis(tickers, el);
         browser.storage.local.set( { unrealizedCostBasis: {
           data: unrealizedCostBasis,
           timestamp: new Date().toLocaleString('en-US')
         }});
         browser.runtime.sendMessage({ type: 'unrealized-success' });
-        haveRefreshed = true;
+        haveRefreshedUnrealized = true;
       });
     } catch(error) {
       browser.runtime.sendMessage({ type: 'unrealized-error', msg: error});
       console.error(error);
     }
+  } else if (msg.type === 'refresh-realized') {
+    try {
+      if (haveRefreshedRealized) {
+        throw 'Can only refresh cost basis once between page refreshes';
+      }
+      const tickers = parseTickers(false);
+      fetchRealizedCostBasis().then(el => {
+        const realizedCostBasis = parseRealizedCostBasis(tickers, el);
+        browser.storage.local.set( { realizedCostBasis: {
+          data: realizedCostBasis,
+          timestamp: new Date().toLocaleString('en-US')
+        }});
+        browser.runtime.sendMessage({ type: 'realized-success' });
+        haveRefreshedRealized = true;
+      });
+    } catch(error) {
+      browser.runtime.sendMessage({ type: 'realized-error', msg: error});
+      console.error(error);
+    }
   }
 });
 
-function parseData(tickers, el) { 
+function parseDollars(text) {
+  const replaced = text
+    .replace('$', '')
+    .replace(',', '')
+    .replace(' ', '')
+    .replace('\u2013', '-');
+  return Number.parseFloat(replaced);
+}
+
+function parseUnrealizedCostBasis(tickers, el) { 
   const tables = Array.from(el.querySelectorAll('table.dataTable'));
   return tables.flatMap( (table, idx) => {
     const ticker = tickers[idx];
     const rows = Array.from(table.querySelectorAll('tr')).slice(1);
-    console.log('Number of rows', rows.length);
     return rows.flatMap ( (row) => {
       const cells = Array.from(row.querySelectorAll('td'));
       return {
         ticker: ticker,
         date: Date.parse(cells[0].innerText),
-        marketValue: Number.parseFloat(cells[4].innerText.replace('$', '').replace(',', ''))
+        gainOrLoss: parseDollars(cells[7].innerText),
+        marketValue: parseDollars(cells[4].innerText)
       };
     });
   });
 }
 
-function parseTickers() {
-  const elements = document.querySelectorAll('table.dataTable td.noBotBorder:not(.subHead):nth-child(1)');
+function parseRealizedCostBasis(tickers, el) {
+  const tables = Array.from(el.querySelectorAll('table.dataTable'))
+    .filter(t => t.id.includes("noValue"));
+  return tables.flatMap( (table, idx) => {
+    const ticker = tickers[idx];
+    const rows = Array.from(table.querySelectorAll('tr')).slice(1);
+    const rowData =  rows.flatMap ( (row) => {
+      const cells = Array.from(row.querySelectorAll('td'));
+      return {
+        ticker: ticker,
+        dateSold: Date.parse(cells[0].innerText),
+        dateAcquired: Date.parse(cells[1].innerText),
+        gainOrLoss: parseDollars(cells[9].innerText)
+      };
+    });
+    return rowData;
+  });
+}
+
+function parseTickers(unrealized) {
+  const idPrefix = unrealized ? "unrealizedTabForm" : "realizedTabForm";
+  const tables = Array.from(document.querySelectorAll("table.dataTable"))
+    .filter(t => t.id.startsWith(idPrefix));
+  const elements = tables.flatMap(t => Array.from(t.querySelectorAll('td.noBotBorder:not(.subHead):nth-child(1)')));
   const tickers = Array.from(elements).map( (el) => el.innerText);
   return tickers;
 }
 
-function fetchTickerData() {
+function parseIdString(id) {
+  const parts = id.split('_');
+  const acctId = parts[4];
+  const cusip = parts[2];
+  const holdingId = parts[3];
+  const randomTag = parts[5];
+
+  return { acctId, cusip, holdingId, randomTag };
+}
+
+function fetchUnrealizedCostBasis() {
   const idStrings = Array.from(document.querySelectorAll('span.comp-NavBox')).map(q => q.id);
   const promises = idStrings.map( id => {
-    const parts = id.split('_');
-    const acctId = parts[4];
-    const cusip = parts[2];
-    const holdingId = parts[3];
-    const randomTag = parts[5];
-    const url = `https://personal.vanguard.com/us/XHTML/com/vanguard/costbasisnew/xhtml/subview/lotdata.xhtml?cusip=${cusip}&acctId=${acctId}&holdingID=${holdingId}&isMF=false&isShort=false&randomTag=${randomTag}`;
+    const parsedId = parseIdString(id);
+    const url = `https://personal.vanguard.com/us/XHTML/com/vanguard/costbasisnew/xhtml/subview/lotdata.xhtml?cusip=${parsedId.cusip}&acctId=${parsedId.acctId}&holdingID=${parsedId.holdingId}&isMF=false&isShort=false&randomTag=${parsedId.randomTag}`;
     return fetch(url, 
       {
         "credentials": "include",
         "headers": { 
           "accept":"*/*",
-          "accept-language":"en-US,en;q=0.9","adrum":"isAjax:true","cache-control":"no-cache","pragma":"no-cache"},"referrer":"https://personal.vanguard.com/us/XHTML/com/vanguard/costbasisnew/xhtml/CostBasisSummary.xhtml","referrerPolicy":"no-referrer-when-downgrade","body":null,"method":"GET","mode":"cors"})
+          "accept-language":"en-US,en;q=0.9",
+          "adrum":"isAjax:true",
+          "cache-control":"no-cache",
+          "pragma":"no-cache"
+        },
+        "referrer":"https://personal.vanguard.com/us/XHTML/com/vanguard/costbasisnew/xhtml/CostBasisSummary.xhtml",
+        "referrerPolicy":"no-referrer-when-downgrade",
+        "body":null,
+        "method":"GET",
+        "mode":"cors"
+      })
+      .then(r => r.text())
+      .catch(error => console.log(error));
+  });
+  return Promise.all(promises)
+    .then(strings => new DOMParser().parseFromString(strings.join(''), 'text/html'));
+}
+
+function fetchRealizedCostBasis() {
+  const idStrings = Array.from(document.querySelectorAll('span.comp-NavBox')).map(q => q.id)
+    .filter(id => id.includes("comp-realizedTabForm"));
+  const promises = idStrings.map( id => {
+    const parsedId = parseIdString(id);
+    const url = `https://personal.vanguard.com/us/XHTML/com/vanguard/costbasisnew/xhtml/subview/lotdata.xhtml?cusip=${parsedId.cusip}&acctId=${parsedId.acctId}&holdingID=${parsedId.holdingId}&isMF=false&isShort=false&randomTag=${parsedId.randomTag}`;
+    return fetch(url, 
+      {
+        "credentials": "include",
+        "headers": { 
+          "accept":"*/*",
+          "accept-language":"en-US,en;q=0.9",
+          "adrum":"isAjax:true",
+          "cache-control":"no-cache",
+          "pragma":"no-cache"
+        },
+        "referrer":"https://personal.vanguard.com/us/XHTML/com/vanguard/costbasisnew/xhtml/CostBasisSummary.xhtml",
+        "referrerPolicy":"no-referrer-when-downgrade",
+        "body":null,
+        "method":"GET",
+        "mode":"cors"
+      })
       .then(r => r.text())
       .catch(error => console.log(error));
   });
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -7,6 +7,7 @@ $.DataTable = dt;
 showTimestamps();
 maybeDisplayTrades();
 let unrealizedTable = null;
+let realizedTable = null;
 
 browser.runtime.onMessage.addListener( (msg) => {
   console.log('Received message', msg);
@@ -29,12 +30,24 @@ document.getElementById("refreshUnrealized").onclick = (event) => {
   });
 };
 
+document.getElementById("refreshRealized").onclick = (event) => {
+  console.log('Got click, sending message');
+  chrome.tabs.query( { active: true, currentWindow: true }, (tabs) => {
+    browser.tabs.sendMessage(tabs[0].id, { type: 'refresh-realized' })
+  });
+};
+
 document.getElementById("invest").onclick = (event) => {
-  browser.storage.local.get(['unrealizedCostBasis', 'portfolio'])
+  browser.storage.local.get(['unrealizedCostBasis', 'realizedCostBasis', 'portfolio'])
     .then( (obj) => {
-      const df = new DataFrame(obj.unrealizedCostBasis.data);
+      const realizedDf = new DataFrame(obj.realizedCostBasis.data);
+      const unrealizedDf = new DataFrame(obj.unrealizedCostBasis.data);
       const cash = parseFloat(document.getElementById('cash').value);
-      const trades = getInvestments(df, cash, obj.portfolio)
+      /**
+      const unrealizedMinusLosses = unrealizedDf
+        .diff(losses, ['ticker', 'date', 'marketValue', 'gainOrLoss']);
+      */
+      const trades = getInvestments(unrealizedDf, realizedDf, cash, obj.portfolio)
         .map(row => row.set('hide', 'Hide'));
       saveTrades(trades.toArray());
       displayTrades(trades);
@@ -57,14 +70,40 @@ document.getElementById("toggleUnrealized").onclick = event => {
           data: obj.unrealizedCostBasis.data,
           columns: [
             { title: "Ticker", data: "ticker" },
-            { title: "Value", data: "marketValue" },
             { title: "Date", data: "date", render: d => new Date(d).toLocaleDateString("en-US") },
+            { title: "Value", data: "marketValue" },
+            { title: "Gain/Loss", data: "gainOrLoss" }
           ],
           searching: false,
           paging: false,
           order: [[0, 'asc']]
         });
         document.getElementById("toggleUnrealized").textContent = "Hide";
+      });
+  }
+}
+
+document.getElementById("toggleRealized").onclick = event => {
+  if (realizedTable !== null) {
+    destroyDataTable("#realizedTable");
+    realizedTable = null;
+    document.getElementById("toggleRealized").textContent = "Show";
+  } else {
+    browser.storage.local.get(['realizedCostBasis'])
+      .then( obj => {
+        realizedTable = $('#realizedTable').DataTable( {
+          data: obj.realizedCostBasis.data,
+          columns: [
+            { title: "Ticker", data: "ticker" },
+            { title: "Date Acquired", data: "dateAcquired", render: d => new Date(d).toLocaleDateString("en-US") },
+            { title: "Date Sold", data: "dateSold", render: d => new Date(d).toLocaleDateString("en-US") },
+            { title: "Gain/Loss", data: "gainOrLoss" },
+          ],
+          searching: false,
+          paging: false,
+          order: [[0, 'asc']]
+        });
+        document.getElementById("toggleRealized").textContent = "Hide";
       });
   }
 }
@@ -88,21 +127,93 @@ function showTimestamps() {
       const dateStr = obj.unrealizedCostBasis.timestamp;
       document.getElementById('unrealizedLastRefresh').textContent = dateStr;
     });
+
+  browser.storage.local.get(['realizedCostBasis'])
+    .then( obj => {
+      const dateStr = obj.realizedCostBasis.timestamp;
+      document.getElementById('realizedLastRefresh').textContent = dateStr;
+    });
 }
 
-function getInvestments(df, cash, portfolio) {
+/**
+ * Determines which stocks to buy and sell.
+ *
+ * First, examine all holdings and see if there are any qualifying losses (those above the
+ * user-defined threshold that have not been purchased in the last 30 days).
+ *
+ * Second, determine which categories should be bought or sold. Except to lock in losses, we only
+ * make trades in the direction of current investment i.e., buy stocks if investment is positive,
+ * sell if negative.
+ *
+ * Third, for categories that are to be purchased, chose the ticker to buy.
+ */
+function getInvestments(unrealizedDf, realizedDf, cash, portfolio) {
+  const losses = findLossesToHarvest(realizedDf, unrealizedDf);
+
+  // For the rest of this algorithm, assume that we've already sold losses
+  const cashWithLosses = cash - losses.stat.sum('gainOrLoss');
+  const unrealizedMinusLosses = unrealizedDf
+    .diff(losses, ['ticker', 'date', 'marketValue', 'gainOrLoss']);
+  const realizedWithLosses = realizedDf
+    .union(losses.withColumn('dateSold', () => Date.now())
+      .rename('date', 'dateAcquired')
+      .drop('marketValue'));
+
+  // Create an empty holding for each category so that we still calculate stats for categories that
+  // don't yet have holdings
+  const emptyCategoryDf = new DataFrame(Object.keys(portfolio)
+    .map(cat => [cat]), ['category']).withColumn('marketValue', () => 0);
+
+  // Annotate each holding by the category to which it belongs
+  const withCat = unrealizedDf.map(row => row.set('category', getCategory(row.get('ticker'), portfolio)))
+    .select('category', 'marketValue')
+    .union(emptyCategoryDf);
+
+  // Determine how much the allocation of each category should change
   const floorFn = n => cash > 0 ? Math.max(n, 0) : Math.min(n, 0);
-  const withCat = df.map(row => row.set('category', getCategory(row.get('ticker'), portfolio)));
-  const totalValue = df.stat.sum('marketValue');
+  const totalValue = unrealizedDf.stat.sum('marketValue');
   const deltaByCat = withCat.groupBy('category').aggregate(cat => cat.stat.sum('marketValue'), 'totalValue')
     .map(row => row.set('desiredValue', portfolio[row.get('category')].allocation * (totalValue + cash)))
     .map(row => row.set('desiredDelta', floorFn(row.get('desiredValue') - row.get('totalValue'))));
 
+  // Normalize by the total amount of investment
   const totalDelta = deltaByCat.stat.sum('desiredDelta');
-  const categoriesToBuy = deltaByCat.map(row => row.set('delta', row.get('desiredDelta') * (cash / totalDelta)))
-    .filter(row => row.get('delta') != 0);
-  return categoriesToBuy.map(row => row.set('ticker', chooseTicker(row.get('category'), portfolio)))
-    .select('ticker', 'delta');
+  const normalizedDeltaByCat = deltaByCat
+    .map(row => row.set('delta', row.get('desiredDelta') * (cash / totalDelta)))
+    .select('category', 'delta');
+
+  console.log('normalizedDelta', normalizedDeltaByCat.toArray());
+  const categoriesToBuy = normalizedDeltaByCat.filter(r => r.get('delta') > 0);
+  const categoriesToSell = normalizedDeltaByCat.filter(r => r.get('delta') < 0);
+  const recentLosses = realizedDf.filter(row => row.get('dateSold') > daysAgo(30));
+
+  return {
+    toBuy: chooseTickersToBuy(categoriesToBuy, portfolio, recentLosses),
+    toSell: chooseHoldingsToSell(losses, categoriesToSell, unrealizedMinusLosses, portfolio)
+  };
+}
+
+function daysAgo(days) {
+  return Date.now() - 1000 * 60 * 60 * 24 * 30;
+}
+
+function removeRecentTransactions(realizedDf, unrealizedDf) {
+  console.log(daysAgo(30));
+  const recentTransactions = 
+    unrealizedDf.filter(row => row.get('date') > daysAgo(30)).select('ticker')
+      .union(realizedDf.filter(row => row.get('dateAcquired') > daysAgo(30)).select('ticker'));
+  console.log('recentTransactions', recentTransactions.toArray());
+  return unrealizedDf.diff(recentTransactions, 'ticker');
+}
+
+function findLossesToHarvest(realizedDf, unrealizedDf) {
+  const eligibleHoldings = removeRecentTransactions(realizedDf, unrealizedDf);
+  console.log('eligibleHoldings', eligibleHoldings);
+  const tickersToHarvest = eligibleHoldings
+    .filter(row => row.get('gainOrLoss') < -200)
+    .distinct('ticker');
+  return eligibleHoldings.filter(row => row.get('gainOrLoss') < 0)
+    .leftJoin(tickersToHarvest, 'ticker');
 }
 
 function getCategory(ticker, portfolio) {
@@ -115,16 +226,43 @@ function getCategory(ticker, portfolio) {
   return category;
 }
 
-function chooseTicker(category, portfolio) {
-  return portfolio[category].tickers[0];
+/**
+ * Choose the ticker to buy for each category. For each category, choose the ticker appearing first
+ * in the portfolio's list of tickers that has not been sold for a loss in the last 30 days (to
+ * avoid the wash sale rule).
+ *
+ * TODO(hhd): We should be smarter here choose holdings that we bought most recently
+ */
+function chooseTickersToBuy(categoriesToBuy, portfolio, recentLosses) {
+  const tickersToBuy = categoriesToBuy.map( row => {
+    const category = row.get('category');
+    const firstEligibleTicker = portfolio[category].tickers
+      .find(el => !recentLosses.find(row => row.get('ticker') === el))
+
+    // If there's no eligible ticker, just buy the first one
+    const ticker = firstEligibleTicker ? firstEligibleTicker : portfolio[category].tickers[0];
+    return row.set('ticker', ticker);
+  }).select('ticker', 'delta');
+  return tickersToBuy;
+}
+
+/**
+ * Choose the holdings to sell for each category.
+ *
+ * Prefers holdings which
+ * 1) are long term capital gains
+ * 2) are already being sold as part of the losses
+ * 3) appear first in the list of tickers for the category
+ */
+function chooseHoldingsToSell(losses, unrealizedMinusLosses, categoriesToSell, portolio) {
+
 }
 
 function maybeDisplayTrades() {
   browser.storage.local.get(['suggestedTrades'])
     .then( obj => {
       if (obj.suggestedTrades) {
-        console.log(obj.suggestedTrades);
-        displayTrades(new DataFrame(obj.suggestedTrades));
+        displayTrades(obj.suggestedTrades);
       } else {
         clearTrades();
       }
@@ -139,7 +277,7 @@ function displayTrades(tradesDf) {
     data: tradesDf.toArray(),
     columns: [ 
       { title: 'Ticker' }, 
-      { title: 'Delta' }, 
+      { title: '$' }, 
       { title: 'Action', className: 'hide' }],
     searching: false,
     paging: false,
@@ -157,6 +295,7 @@ function displayTrades(tradesDf) {
 function clearTrades() {
   document.getElementById("investDiv").hidden = false;
   document.getElementById("clearTradesDiv").hidden = true;
-  destroyDataTable("#tradesTable");
+  destroyDataTable("#sellTable");
+  destroyDataTable("#buyTable");
   browser.storage.local.remove(['suggestedTrades']);
 }
